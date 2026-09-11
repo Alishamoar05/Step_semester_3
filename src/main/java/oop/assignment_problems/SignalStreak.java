f8c7c5a14f0928f5250abe8121fb5c29af7feb7a
Alishamoar05 <alishamoar587@gmail.com>
2026-09-11 22:22:52 +0530

    Add Week 1 assignment problems

---
+++ src/main/java/oop/assignment_problems/SignalStreak.java
@@ -0,0 +1,48 @@
+package main.java.oop.assignment_problems;
+
+import java.util.Scanner;
+
+public class SignalStreak {
+
+    static void findLongestStreak(String signalLog) {
+        if (signalLog.length() == 0) {
+            System.out.println("No Signal Data");
+            return;
+        }
+
+        char longestColor = signalLog.charAt(0);
+        int longestLength = 1;
+
+        char currentColor = signalLog.charAt(0);
+        int currentLength = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+            if (signalLog.charAt(i) == currentColor) {
+                currentLength++;
+            } else {
+                currentColor = signalLog.charAt(i);
+                currentLength = 1;
+            }
+
+            if (currentLength > longestLength) {
+                longestLength = currentLength;
+                longestColor = currentColor;
+            }
+        }
+
+        System.out.println("Longest Streak: '" + longestColor
+                + "' repeated " + longestLength + " times");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter signal log: ");
+        String signalLog = scanner.nextLine();
+
+        findLongestStreak(signalLog);
+
+        scanner.close();
+    }
+}
